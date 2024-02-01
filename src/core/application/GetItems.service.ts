018baf6be022c3e272b0261d91e68fe3d2c8f118
pabloppizarro <pizarro.pablo@outlook.com>
2024-02-01 15:44:15 -0300

    get items fixed return type.

--- src/core/application/GetItems.service.ts
+++ src/core/application/GetItems.service.ts
@@ -5,17 +5,14 @@ import { IItemRepository } from "./repositories/IItemsRepository";
 export class GetItemsService {
   public constructor(private readonly _itemsRepo: IItemRepository) {}
 
-  public async getItems(searchKey: string): Promise<IGetItems | []> {
-    const itemsResult = await this._itemsRepo
-      .getItems(searchKey)
-      .catch((err) => {
-        throw err;
-      });
+  public async getItems(searchKey: string): Promise<IGetItems> {
+    const itemsResult = await this._itemsRepo.getItems(searchKey);
+    //MELI API RESPONDS AN OBJECT WITH A ERROR Property
+    if (itemsResult.error) {
+      throw JSON.stringify(itemsResult);
+    }
     const { results } = itemsResult;
 
-    if (!results.length) {
-      return [];
-    }
     const categories = results.map((item: any) => item.category_id);
 
     const items: IItem[] = results.map((item: any) => ({
