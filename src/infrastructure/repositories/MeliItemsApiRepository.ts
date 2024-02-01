5da611943b82f8f2c2721bd0690a96c6c47044b3
pabloppizarro <pizarro.pablo@outlook.com>
2024-02-01 14:33:01 -0300

    implemented detail and description in meli repository

--- src/infrastructure/repositories/MeliItemsApiRepository.ts
+++ src/infrastructure/repositories/MeliItemsApiRepository.ts
@@ -3,6 +3,21 @@ import { IItemRepository } from "../../core/application/repositories/IItemsRepos
 export class MeliItemsApiRepository implements IItemRepository {
   public constructor() {}
 
+  public async getItemDetail(id: string): Promise<any> {
+    return fetch(`${process.env.MELI_ITEMS_API}/items${id}/description`)
+      .then((res) => res.json())
+      .catch((err) => {
+        throw err;
+      });
+  }
+  public async getItemDescription(id: string): Promise<any> {
+    return fetch(`${process.env.MELI_ITEMS_API}/items${id}`)
+      .then((res) => res.json())
+      .catch((err) => {
+        throw err;
+      });
+  }
+
   public async getItems(searchKey: string): Promise<any> {
     return fetch(
       `${process.env.MELI_ITEMS_API}/sites/MLA/search?q=${searchKey}`
