302aca8a93bd9ef6ffba085bd9929efa93e7ca2d
pabloppizarro <pizarro.pablo@outlook.com>
2024-02-01 13:59:10 -0300

    fixed json response from fetch

--- src/infrastructure/repositories/MeliItemsApiRepository.ts
+++ src/infrastructure/repositories/MeliItemsApiRepository.ts
@@ -1,11 +1,15 @@
 import { IItemRepository } from "../../core/application/repositories/IItemsRepository";
 
 export class MeliItemsApiRepository implements IItemRepository {
-  protected constructor() {}
+  public constructor() {}
 
   public async getItems(searchKey: string): Promise<any> {
     return fetch(
-      `${process.env.MELI_ITEMS_API}/sites/MLA/search?=${searchKey}`
-    );
+      `${process.env.MELI_ITEMS_API}/sites/MLA/search?q=${searchKey}`
+    )
+      .then((res) => res.json())
+      .catch((err) => {
+        throw err;
+      });
   }
 }
