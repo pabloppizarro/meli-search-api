25b4a3c0097f4d9c507d53960302536f849a8ada
pabloppizarro <pizarro.pablo@outlook.com>
2024-02-01 13:59:25 -0300

    added get items use case workflow

--- src/core/application/GetItems.service.ts
+++ src/core/application/GetItems.service.ts
@@ -3,37 +3,37 @@ import { IItem } from "./interfaces/IItem";
 import { IItemRepository } from "./repositories/IItemsRepository";
 
 export class GetItemsService {
-  protected constructor(private readonly _itemsRepo: IItemRepository) {}
+  public constructor(private readonly _itemsRepo: IItemRepository) {}
 
   public async getItems(searchKey: string): Promise<IGetItems | []> {
-    const itemsResult = await this._itemsRepo.getItems(searchKey);
+    const itemsResult = await this._itemsRepo
+      .getItems(searchKey)
+      .catch((err) => {
+        throw err;
+      });
     const { results } = itemsResult;
 
-    if (!results.listen) {
+    if (!results.length) {
       return [];
     }
-    try {
-      const categories = results.map((item: any) => item.category_id);
+    const categories = results.map((item: any) => item.category_id);
 
-      const items: IItem[] = results.map((item: any) => ({
-        id: item.id,
-        title: item.title,
-        price: {
-          currency: item.currency_id,
-          amount: item.price,
-          decimals: 2, //??
-        },
-        picture: item.thumbnail,
-        condition: item.condition,
-        free_shipping: item.shipping.free_shipping,
-      }));
-      return {
-        author: { name: "Pachu", lastName: "Pizarro" },
-        categories,
-        items,
-      };
-    } catch (error) {
-      throw error;
-    }
+    const items: IItem[] = results.map((item: any) => ({
+      id: item.id,
+      title: item.title,
+      price: {
+        currency: item.currency_id,
+        amount: item.price,
+        decimals: 2, //??
+      },
+      picture: item.thumbnail,
+      condition: item.condition,
+      free_shipping: item.shipping.free_shipping,
+    }));
+    return {
+      author: { name: "Pachu", lastName: "Pizarro" },
+      categories,
+      items,
+    };
   }
 }
--- src/infrastructure/Items.route.ts
+++ src/infrastructure/Items.route.ts
@@ -1,11 +1,29 @@
 import { Router } from "express";
+import { MeliItemsApiRepository } from "./repositories/MeliItemsApiRepository";
+import { GetItemsService } from "../core/application/GetItems.service";
+
+const meliRepo = new MeliItemsApiRepository();
+const getItemsService = new GetItemsService(meliRepo);
 
 const router = Router();
 
 router.route("/").get((req, res) => {
   const { q } = req.query;
-  res.status(200).json({ query: q });
+
+  if (!q || typeof q !== "string") {
+    res.status(400).send("Query string not found");
+  } else {
+    getItemsService
+      .getItems(q)
+      .then((itemsResult) => {
+        res.status(200).json(itemsResult);
+      })
+      .catch((err) => {
+        throw res.status(500).send({ msg: "Something wrong", err });
+      });
+  }
 });
+
 router.route("/:id").get((req, res) => {
   res.status(200).json({ id: req.params.id });
 });
