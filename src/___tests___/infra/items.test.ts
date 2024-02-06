ba0a2861ae70164b76b39b4d89c3b536c495c5fc
pabloppizarro <pizarro.pablo@outlook.com>
2024-02-06 13:37:14 -0300

    added test for min and max char validation. test code 200 for correct search text lenght.

--- src/___tests___/infra/items.test.ts
+++ src/___tests___/infra/items.test.ts
@@ -1,12 +1,41 @@
 import supertest from "supertest";
 import { ApiServer } from "../../infrastructure/ApiServer";
+
+const app = ApiServer.run(3001);
+
 describe("Items", () => {
   describe("get items routes", () => {
     describe("given a empty search query", () => {
-      it("should return a 404 status code", async () => {
+      it("should return a 400 status code", async () => {
         const searchKey: string = "";
-        const app = await ApiServer.run(3001);
-        await supertest(app).get(`/api/items?search=${searchKey}`).expect(400);
+        await supertest(await app)
+          .get(`/api/items?search=${searchKey}`)
+          .expect(400);
+      });
+    });
+    describe("given a search query", () => {
+      describe("contain less than 3 chars", () => {
+        it("should return a 400 status code", async () => {
+          const searchKey: string = "ap";
+          await supertest(await app)
+            .get(`/api/items?search=${searchKey}`)
+            .expect(400);
+        });
+      });
+      describe("contain more than 120 chars", () => {
+        it("should return a 400 status code", async () => {
+          const searchKey: string =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur gravida molestie egestas. Cras tempor magna tristique convallis. ";
+          await supertest(await app)
+            .get(`/api/items?search=${searchKey}`)
+            .expect(400);
+        });
+      });
+      it("should return a 200 status code", async () => {
+        const searchKey: string = "apple";
+        await supertest(await app)
+          .get(`/api/items?search=${searchKey}`)
+          .expect(200);
       });
     });
   });
